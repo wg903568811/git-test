b2ebd0b04d72a38b1dd62b416591494136a1b471
wuge <903568811@qq.com>
2021-11-24 16:45:48 +0800

    提交git到本地仓库

---
+++ git-wuge/src/com/wu/git.java
@@ -0,0 +1,11 @@
+package com.wu;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
